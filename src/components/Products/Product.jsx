be861bcab6386e5a5787a962c1f3826ba814ed09
mahedi6666655555 <mahedinir34678@gmail.com>
2023-03-27 00:13:17 +0600

    website is ready to lonch in the indestry

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -5,8 +5,8 @@ const Product = (props) => {
   let { img, name, seller, quantity, ratings, price } = props.product;
   // console.log(quantity);
   return (
-    <div className="w-[290px] mt-20 h-[510px] mm relative ">
-      <img src={img} alt="" className="w-[286px] p-2 mx-auto" />
+    <div className="w-[350px] mt-20 h-[600px] mm relative ">
+      <img src={img} alt="" className=" p-2 mx-auto" />
       <div className="pl-4 pt-4">
         <h6 className="Product-name">{name}</h6>
         <h5 className="pt-2 text-2xl font-bold">Price:{price} </h5>
@@ -15,7 +15,9 @@ const Product = (props) => {
           <p>Rating: {ratings} star</p>
         </div>
       </div>
-      <button className="w-full  bg-yellow-400  bottom-0 rounded-lg py-4 absolute hover:bg-red-400">add to cart</button>
+      <button className="w-full  bg-yellow-400  bottom-0 rounded-lg py-4 absolute hover:bg-red-400">
+        add to cart
+      </button>
     </div>
   );
 };
